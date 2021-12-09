524a598f12648c78dfcd614eed0f16ded1b429af
sofian salah <sofiansofian2305@gmail.com>
2021-12-09 22:00:24 +0100

    final push

---
+++ src/views/Character.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <p>Nom: {{this.character.name}},<br>
+          Poids: {{this.character.mass}}, <br>
+          Couleur des yeux: {{this.character.eye_color}}
+        </p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: 'Character',
+  
+    data() {
+        return {
+            character: {},
+        }
+    },
+    created() {
+        if (this.$route.params.id) {
+            axios.get("https://swapi.dev/api/people/" + this.$route.params.id + "/")
+            .then(res => {
+                this.character = res.data
+            })
+        }
+    }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+    <People v-bind:characters="this.characters" @characterDetail="characterDetail" @searchCharacter="searchCharacter"/>
+  </div>
+</template>
+
+
+<script>
+import axios from 'axios'
+import People from '../components/People.vue'
+export default {
+  name: 'Home',
+  components: {
+    People,
+  },
+  
+  data() {
+    return {
+      characters: [],
+    }
+  },
+  methods: {
+    characterExist(name) {
+      var selected = null;
+      this.characters.forEach(c => {
+        if (c.name == name) {selected = c}
+      });
+      return selected;
+    },
+    characterDetail(url) {
+      var id = url.slice(29, url.length - 1);
+      window.location.href = 'http://localhost:8080/character/' + id;
+    },
+    searchCharacter(input) {
+      var url = "https://swapi.dev/api/people/?search=" + input
+      axios.get(url)
+      .then(res => {this.characters = res.data.results})
+      
+    }
+  },
+  created() {
+    axios.get("https://swapi.dev/api/people/")
+    .then(res => {this.characters = res.data.results;})
+    
+  }
+}
+</script>
